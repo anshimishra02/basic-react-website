36560e88a9083a12715cef5fdfaa59f102e0b599
anshimishra02 <anshimishra.0207@gmail.com>
2021-08-13 16:37:57 +0530

    first commit

---
+++ src/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact =() => {
+    return (
+        <>
+        <h1>Welcome to my Contact page</h1>
+        </>
+    );
+};
+
+export default Contact;
---
+++ src/Destinations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Destinations =() => {
+    return (
+        <>
+        <h1>Welcome to my Destinations page</h1>
+        </>
+    );
+};
+
+export default Destinations;
